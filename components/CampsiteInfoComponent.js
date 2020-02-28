dd908ab9455d38559ac786ab6d8f3e7ace0b028a
Sarzana Maldonado <sarzanam925@gmail.com>
2020-02-27 23:39:38 -0500

    Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -93,6 +93,17 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
+
+
     if (campsite) {
         return (
             <Animatable.View 
@@ -126,6 +137,16 @@ function RenderCampsite(props) {
                     onPress={() => props.onShowModal()} 
                     style={styles.cardItem}
                 />
+                <Icon
+                name={'share'}
+                type='font-awesome'
+                color='#5637DD'
+                style={styles.cardItem}
+                raised
+                reversed
+                onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
+                />
+
                 </View>
             </Card>
             </Animatable.View>
